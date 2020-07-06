70e87069a4906bc00bad5185a19508c215fbef9d
Patrick Mitchener <pmitchener@rogers.com>
2020-07-06 00:49:41 +0000

    hello.js and even.js

--- hello.js
+++ hello.js
@@ -1,6 +1,13 @@
-const sayHello  = function (name) {
+/*const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
 sayHello("Caliban");
 sayHello("Miranda");
-sayHello("Ferdinand");
+sayHello("Ferdinand");*/
+
+//function return
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('Caliban');
+console.log(greeting);
